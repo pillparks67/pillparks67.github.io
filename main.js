e14e03fe7d5fe70db127cccea15ba273c0ce04a1
lua <88602791+luavmx@users.noreply.github.com>
2026-02-20 10:14:08 +1000

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function init() {
     scene = new THREE.Scene()
 
     camera = new THREE.PerspectiveCamera(
-        90, // fov
+        45, // fov
         window.innerWidth / window.innerHeight, // aspect ratio
         .1, // near plane
         1000 // far plane
@@ -15,32 +15,41 @@ function init() {
     renderer = new THREE.WebGLRenderer()
     renderer.setClearColor(0x000000, 1.0)
     renderer.setSize(window.innerWidth, window.innerHeight)
+    // six sevennnn
+    const loader = new THREE.TextureLoader();
+    loader.load("hkh.png", (texture) => {
+        let cubeGeometry = new THREE.BoxGeometry(9, 9, 9)
+        let material = new THREE.MeshBasicMaterial({map: texture})
+        let cube = new THREE.Mesh(cubeGeometry, material)
+        cube.name = "cube"
+        
+        scene.add(cube)
+    })
 
-    camera.position.x = 0
-    camera.position.y = 50
-    camera.position.z = 50
+    camera.position.x = 15
+    camera.position.y = 15
+    camera.position.z = 15
 
     camera.lookAt(scene.position)
 
     document.body.appendChild(renderer.domElement)
 
-    addFloor("floor")
-    addLight()
-
-    render()
+    requestAnimationFrame(render)
 }
 
-let time = 0
-
-function render() {
+function render(time) {
     renderer.render(scene, camera)
 
-    let floor = scene.getObjectByName("floor")
-    floor.position.z = -Math.tan(time/3) * 5
-    //floor.rotation.y = Math.sin(time) / 3
-    floor.rotation.z = Math.cos(time/3) / 3
+    time *= 0.001
 
-    time += 0.17
+    let cube = scene.getObjectByName("cube")
+
+    if (cube) {
+        cube.rotation.x = time
+        cube.rotation.z = time
+        cube.rotation.y = time
+        cube.position.y = -2.5 + Math.abs(Math.sin(time * 3) * 5)
+    }
 
     requestAnimationFrame(render)
 }
@@ -50,35 +59,6 @@ function resize() {
     camera.updateProjectionMatrix()
 
     renderer.setSize(window.innerWidth, window.innerHeight)
-}
-
-function toRadians(degrees) {
-    return degrees * Math.PI / 180
-}
-
-function addFloor(name) {
-    let geometry = new THREE.PlaneGeometry(100, 100, 20, 20)
-
-    let material = new THREE.MeshPhongMaterial()
-    material.map = THREE.ImageUtils.loadTexture("hkh.png")
-    material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping
-    material.map.repeat.set(1, 1)
-
-    let mesh = new THREE.Mesh(geometry, material)
-    mesh.rotation.set(toRadians(-45), 0, 0)
-    mesh.receiveShadow = true
-    mesh.name = name
-
-    scene.add(mesh)
-}
-
-function addLight() {
-    let light = new THREE.SpotLight()
-    light.position.set(0, 100, 30)
-    light.brightness = 100
-    light.castShadow = true
-
-    scene.add(light)
 }
 
 window.addEventListener("load", init)
